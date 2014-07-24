2b4ad66ceb71c9c8bd7d3db53969f0f180cc3851
Jarrod Doucette FORS <jdoucett@purdue.edu>
2014-07-24 11:54:18 -0400

    Change named of app, background color of nabvar, and center location and zoom of map.

--- src/js/config.js
+++ src/js/config.js
@@ -1,17 +1,16 @@
-define([],function() {
-	return {
-    map: {
-      options: {
-        basemap: 'gray',
-        center: [-117.1,33.6],
-        zoom: 9
-      },
-      // TODO: add basemaps
-      basemaps: {
-      }
-    },
-    about: {
-      moreInfoUrl: 'https://github.com/Esri/dojo-bootstrap-map-js'
-    }
-  };
+define([], function() {
+    return {
+        map: {
+            options: {
+                basemap: 'gray',
+                center: [-87.2, 43.5],
+                zoom: 8
+            },
+            // TODO: add basemaps
+            basemaps: {}
+        },
+        about: {
+            moreInfoUrl: 'https://github.com/Esri/dojo-bootstrap-map-js'
+        }
+    };
 });
